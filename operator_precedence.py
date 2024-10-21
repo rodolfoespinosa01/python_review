dc8f4b09be07b1fc58bd7f06c788993efd6611e5
Rodolfo Espinosa-Nunez <rodolfoespinosa01@gmail.com>
2024-10-21 16:41:49 -0400

    Going over Math operator precedence

---
+++ operator_precedence.py
@@ -0,0 +1,9 @@
+# Python uses the simple order of operations
+
+x = 10 + 3 * 2 ** 2
+
+print(x)
+
+# 2 ** 2 represents 2 to the power of 2 - after multiplication hapens, then we add 10
+
+# exponentiation - multiplication or division - addition or subtraction --- parenthesis takes priority
